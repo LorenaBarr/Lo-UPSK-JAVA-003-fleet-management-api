9b2747a0a265718c2cba1ad9ae081d466a3f32cd
LoreBarrera <karenlbarrera@gmail.com>
2024-02-29 12:44:39 -0500

    Configuracion swagger y documentacion

--- src/main/java/com/example/demo/controllers/TaxisController.java
+++ src/main/java/com/example/demo/controllers/TaxisController.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 @RestController
@@ -14,6 +19,12 @@ public class TaxisController {
 
     @Autowired
     private TaxiService taxiService;
+
+    @Operation(summary = "Obtener todos los taxis")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se encontraron todos los taxis",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Taxi.class)) }) })
 
     @GetMapping
     public List<Taxi> getAllTaxis() {
--- src/main/java/com/example/demo/controllers/TrajectoriesController.java
+++ src/main/java/com/example/demo/controllers/TrajectoriesController.java
@@ -6,6 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+
 import java.util.List;
 
 @RestController
@@ -14,6 +22,12 @@ public class TrajectoriesController {
 
     @Autowired
     private TrajectoryService trajectoryService;
+
+    @Operation(summary = "Obtener todas las trayectorias")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se encontraron todas las trayectorias",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Trajectory.class)) }) })
 
     @GetMapping
     public List<Trajectory> getAllTrajectories() {

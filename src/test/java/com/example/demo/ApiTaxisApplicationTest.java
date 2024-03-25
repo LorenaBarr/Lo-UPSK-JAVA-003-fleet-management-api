a5ab9fb5e4434ce74bab61330100ab32a5d6d8c8
LoreBarrera <karenlbarrera@gmail.com>
2024-03-25 18:37:43 -0500

    test funcionales, historia de usuario 4

---
+++ src/test/java/com/example/demo/ApiTaxisApplicationTest.java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.example.demo.repositories.TaxiRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+class ApiTaxisApplicationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TaxiRepository taxiRepository;
+
+
+
+
+
+    @Test
+    @DisplayName("Probando el primer endpoint: Tex...")
+    void context() throws Exception {
+        mockMvc.perform(get("/taxi/all?page=1")).andExpect(status().isOk());
+
+    }
+
+    @Test
+    @DisplayName("test Trajectories")
+    void testControllerTrajectories() throws Exception {
+        String taxi_id = "6418";
+        String dateString = "2008-02-02";
+        String intPage ="0";
+        String pageSize = "10";
+
+        MvcResult mvcResult = this.mockMvc.perform(get("/trajectories/byDateAndId")
+                        .param("taxi_id", taxi_id)
+                        .param("date", dateString)
+                        .param("intPage", intPage)
+                        .param("pageSize", pageSize))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.pageable.pageNumber").value(intPage))
+                .andExpect(jsonPath("$.pageable.pageSize").value(pageSize))
+                .andReturn();
+        assertEquals("application/json", mvcResult.getResponse().getContentType());
+
+    }
+
+}
--- src/test/java/com/example/demo/repositories/TrajectoriesRepositoryTest.java
+++ src/test/java/com/example/demo/repositories/TrajectoriesRepositoryTest.java
@@ -2,52 +2,53 @@ package com.example.demo.repositories;
 
 import com.example.demo.model.Trajectory;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@WebAppConfiguration
 @SpringBootTest
-public class TrajectoriesRepositoryTest {
+@AutoConfigureMockMvc
+class TrajectoriesRepositoryTest {
 
     @Autowired
+    private MockMvc mvc;
+
+    @MockBean
     private TrajectoriesRepository trajectoriesRepository;
 
     @Test
-    void testFindByTaxiIdAndDate() {
-        // Datos de prueba
-        int taxiId = 9275;
-        String date = "2008-02-02";
-        Pageable pageable = PageRequest.of(0, 1);
+    void testLastTrajectories() throws Exception{
+        Pageable pageable = Pageable.ofSize(1).withPage(1);
+        List<Trajectory> trajectories = new ArrayList<>();
+        trajectories.add(new Trajectory());
+        trajectories.add(new Trajectory());
+        trajectories.add(new Trajectory());
+        when (trajectoriesRepository.findLastLocation()).thenReturn(trajectories);
 
-        // Ejecutar método findByTaxiIdAndDate
-        Page<Trajectory> trajectoriesPage = trajectoriesRepository.findByTaxiIdAndDate(taxiId, date, pageable);
+        this.mvc.perform(get("/trajectories/last-Location")
 
-        // Convertir la página de trayectorias a una lista
-        assertNotNull(trajectoriesPage);
-        List<Trajectory> trajectories = trajectoriesPage.getContent();
+                        .param("Page", "1")
+                        .param("Size", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content.length()").value(trajectories.size()))
+                .andReturn();
 
-        // Verificar si se encontraron trayectorias
-        assertNotNull(trajectories);
-        // Aquí puedes agregar más aserciones para verificar los detalles de las trayectorias encontradas
+
+
     }
 
-    @Test
-    void testFindLastLocation() {
-        // Ejecutar método findLastLocation
-        List<Trajectory> lastLocations = trajectoriesRepository.findLastLocation();
-
-        // Verificar si se encontraron las últimas ubicaciones
-        assertNotNull(lastLocations);
-        // Aquí puedes agregar más aserciones para verificar los detalles de las últimas ubicaciones encontradas
-    }
 }
